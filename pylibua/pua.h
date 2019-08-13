72628aaebfb4895a1ddb2f4d80d142a84c50aca1
Vincent Chiu <vincent.chiu@excelfore.com>
2019-08-13 16:26:12 -0700

    Initial Python3 compatible version of python-libua

--- pylibua/pua.h
+++ pylibua/pua.h
@@ -10,6 +10,23 @@
 
 #include <Python.h>
 #include <xl4ua.h>
+#include <unistd.h>
+
+
+#define _ltime_ \
+        char __now[24]; \
+        struct tm __tmnow; \
+        struct timeval __tv; \
+        memset(__now, 0, 24); \
+        gettimeofday(&__tv, 0); \
+        localtime_r(&__tv.tv_sec, &__tmnow); \
+        strftime(__now, 20, "%m-%d:%H:%M:%S.", &__tmnow); \
+        sprintf(__now+15, "%03d", (int)(__tv.tv_usec/1000))
+
+#define PY_DBG(a,b ...)     do { { \
+				      _ltime_; \
+				      printf("[%s] %s:%d " a, __now, chop_path(__FILE__), __LINE__, ## b); printf("\n");\
+			      } } while (0)
 
 typedef struct py_ua_cb {
 	char* ua_get_version;
@@ -17,8 +34,11 @@ typedef struct py_ua_cb {
 	char* ua_pre_install;
 	char* ua_install;
 	char* ua_post_install;
-    char* ua_prepare_install;
+	char* ua_prepare_install;
 	char* ua_prepare_download;
+	char* ua_transfer_file;
+	char* ua_dmc_presence;
+	char* ua_on_message;
 }py_ua_cb_t;
 
 int pua_get_version(const char* type, const char* pkgName, char** version);
@@ -28,10 +48,21 @@ install_state_t pua_prepare_install(const char* type, const char* pkgName, const
 install_state_t pua_pre_install(const char* type, const char* pkgName, const char* version, const char* pkgFile);
 install_state_t pua_install(const char* type, const char* pkgName, const char* version, const char* pkgFile);
 void pua_post_install(const char* type, const char* pkgName);
+int pua_dmc_presence(dmc_presence_t* dp);
+int pua_transfer_file(const char* type, const char* pkgName, const char* version, const char* pkgFile, char** newFile);
+int pua_on_message(const char* msgType, void* message);
 void pua_set_callbacks(PyObject* class_instance, py_ua_cb_t* cb);
 
-int pua_start(char* node_type, ua_cfg_t *cfg);
+int pua_start(char* node_type, ua_cfg_t* cfg);
 int pua_send_message(char* message);
 void pua_end(void);
+
+static inline const char* chop_path(const char* path)
+{
+	const char* aux = strrchr(path, '/');
+
+	if (aux) { return aux + 1; }
+	return path;
+}
 
 #endif /* PUA_H_ */
