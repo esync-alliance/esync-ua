1be3e83dcebf9dc21462d5386090e5bfd58cb3cf
Vincent Chiu <vincent.chiu@excelfore.com>
2020-05-24 13:00:21 -0700

    Added unit test case for fake rb version

--- unit_tests/ut_main.c
+++ unit_tests/ut_main.c
@@ -10,17 +10,27 @@
 #include "test_setup.h"
 #include "ut_updateagent.h"
 
+typedef void (*ut_test_func)(void** state);
+
 extern char* optarg;
 char* handler_type;
 static char case_dir[PATH_MAX];
+static char *cases[] = {
+	"test_normal_update_success",
+	"test_normal_update_failure",
+	"test_delta_dmc_rollback_success",
+	"test_delta_dmc_rollback_failure",
+	"test_rollback_mixed",
+	"test_rollback_fake_version",
+};
 
-static void handle_messages_from_file(char* filename)
+static void handle_messages_from_file(char* filename, ua_cfg_t* cfg)
 {
 	char bus_message[2048];
 	FILE* fd = 0;
 	char fullpath[PATH_MAX];
 
-	ua_cfg_t* cfg = NULL;
+	//ua_cfg_t* cfg = NULL;
 
 	sprintf(fullpath, "%s/%s", case_dir, filename);
 	test_ua_setup((void**)&cfg);
@@ -44,7 +54,7 @@ void test_normal_update_success(void** state)
 	assert_true(!system("rm -rf /data/sota/esync/backup"));
 	assert_true(!system("cp -f ../unit_tests/fixure/backup/ECU_HMNS_ROM_4.0/4.0/ECU_HMNS_ROM-4.0.x /data/sota/tmp/"));
 
-	handle_messages_from_file("ut_normal_update");
+	handle_messages_from_file("ut_normal_update", NULL);
 
 	assert_true(!access("/data/sota/esync/backup/ECU_HMNS_ROM/4.0/ECU_HMNS_ROM-4.0.x", F_OK));
 	assert_true(!access("/data/sota/esync/backup/ECU_HMNS_ROM/manifest_pkg.xml", F_OK));
@@ -57,7 +67,7 @@ void test_normal_update_failure(void** state)
 	assert_true(!system("rm -rf /data/sota/esync/backup"));
 	assert_true(!system("cp -f ../unit_tests/fixure/backup/ECU_HMNS_ROM_4.0/4.0/ECU_HMNS_ROM-4.0.x /data/sota/tmp/"));
 
-	handle_messages_from_file("ut_normal_update_failed");
+	handle_messages_from_file("ut_normal_update_failed", NULL);
 
 	assert_true(access("/data/sota/esync/backup/ECU_HMNS_ROM/4.0/ECU_HMNS_ROM-4.0.x", F_OK));
 	assert_true(access("/data/sota/esync/backup/ECU_HMNS_ROM/manifest_pkg.xml", F_OK));
@@ -76,12 +86,12 @@ void test_rollback_by_dmc(int test_failed)
 	set_test_installation_mode(UPDATE_MODE_ROLLBACK, 0);
 	if (test_failed) {
 		set_rbconf_file("../unit_tests/fixure/rollback/rbFailed");
-		handle_messages_from_file("ut_rollback_by_dmc_failed");
+		handle_messages_from_file("ut_rollback_by_dmc_failed", NULL);
 		assert_true(access("/data/sota/esync/backup/ECU_HMNS_ROM/1.0", F_OK));
 		assert_true(!access("/data/sota/esync/backup/ECU_HMNS_ROM/4.0", F_OK));
 
 	}else {
-		handle_messages_from_file("ut_rollback_by_dmc");
+		handle_messages_from_file("ut_rollback_by_dmc", NULL);
 		assert_true(!access("/data/sota/esync/backup/ECU_HMNS_ROM/1.0/ECU_HMNS_ROM-1.0.x", F_OK));
 		assert_true(!access("/data/sota/esync/backup/ECU_HMNS_ROM/manifest_pkg.xml", F_OK));
 
@@ -113,36 +123,78 @@ void test_rollback_mixed(void** state)
 
 	set_test_installation_mode(UPDATE_MODE_ROLLBACK, 0);
 	set_rbconf_file("../unit_tests/fixure/rollback/rbFailed");
-	handle_messages_from_file("ut_rollback_mixed");
+	handle_messages_from_file("ut_rollback_mixed", NULL);
+
 	assert_true(access("/data/sota/esync/backup/ECU_HMNS_ROM/1.0", F_OK));
-
 	assert_true(!access("/data/sota/esync/backup/ECU_HMNS_ROM/4.0", F_OK));
-
 	assert_true(access("/tmp/esync/ECU_HMNS_ROM/manifest_pkg.xml", F_OK));
 }
+
+void test_rollback_fake_version(void** state)
+{
+	assert_true(!system("rm -rf /data/sota/esync/backup"));
+	assert_true(!system("cp -f ../unit_tests/fixure/backup/ECU_HMNS_ROM-2.0.x /data/sota/tmp/ECU_HMNS_ROM-2.0.x"));
+	assert_true(!system("cp -f ../unit_tests/fixure/backup/ECU_HMNS_ROM_3.0/3.0/ECU_HMNS_ROM-3.0.x /data/sota/tmp/ECU_HMNS_ROM-3.0.x"));
+
+	ua_cfg_t cfg = {0};
+
+	cfg.debug          = 0;
+	cfg.delta          = 1;
+	cfg.reboot_support = 1;
+	cfg.cert_dir       = "/data/sota/pki/certs/ua";
+	cfg.url            = "tcp://localhost:9133";
+	cfg.cache_dir      = "/tmp/esync/";
+	cfg.backup_dir     = "/data/sota/esync/";
+	cfg.delta_config   = &(delta_cfg_t){.delta_cap = "A:4"};
+	
+	set_test_installation_mode(UPDATE_MODE_ROLLBACK, 0);
+	handle_messages_from_file("ut_rollback_fake_version", &cfg);
+
+	assert_true(access("/data/sota/esync/backup/ECU_HMNS_ROM/1.0", F_OK));
+	assert_true(access("/data/sota/esync/backup/ECU_HMNS_ROM/3.0", F_OK));
+	assert_true(access("/tmp/esync/ECU_HMNS_ROM/manifest_pkg.xml", F_OK));
+}
+
+
+static ut_test_func case_funcs[] = {
+	test_normal_update_success,
+	test_normal_update_failure,
+	test_delta_dmc_rollback_success,
+	test_delta_dmc_rollback_failure,
+	test_rollback_mixed,
+	test_rollback_fake_version,
+
+};
 
 static void _help(const char* app)
 {
 	printf("Usage: %s [OPTION...]\n\n%s", app,
 	       "Options:\n"
+	       "  -h         : display this help and exit\n"
 	       "  -t <type>  : handler type (default: \"/UNITTEST/UA/\")\n"
 	       "  -c <path>  : path to test cases directory (default \"../unit_tests/cases/\")\n"
-	       "  -h         : display this help and exit\n"
+	       "  -i index   : run single ith test case, default(0) is to run all tests> \n"
 	       );
+	for (int i = 0; i < sizeof(cases)/sizeof(cases[0]);  i++) {
+		printf("  	       %d =  %s \n", i+1, cases[i]);
+	}
 	_exit(1);
 }
 
 int main(int argc, char** argv)
 {
 	int c = 0;
-
+	int sel = 0;
 	handler_type = UT_DEFAULT_HANDLER_TYPE;
 	strcpy(case_dir, "../unit_tests/cases");
-	while ((c = getopt(argc, argv, ":c:t:h")) != -1) {
+	while ((c = getopt(argc, argv, ":c:i:t:h")) != -1) {
 		switch (c) {
 			case 'c':
 				memset(case_dir, 0, sizeof(case_dir));
 				strncpy(case_dir, optarg, sizeof(case_dir) - 1);
+				break;
+			case 'i':
+				sel = atoi(optarg) - 1;
 				break;
 			case 't':
 				handler_type = optarg;
@@ -154,13 +206,23 @@ int main(int argc, char** argv)
 		}
 	}
 
-	const struct CMUnitTest tests[] = {
-		cmocka_unit_test(test_normal_update_success),
-		cmocka_unit_test(test_normal_update_failure),
-		cmocka_unit_test(test_delta_dmc_rollback_success),
-		cmocka_unit_test(test_delta_dmc_rollback_failure),
-		cmocka_unit_test(test_rollback_mixed),
-	};
+	if(sel) {
+		const struct CMUnitTest signle[] = {
+			cmocka_unit_test(case_funcs[sel]),
+		};
+		return cmocka_run_group_tests(signle, NULL, NULL);
+	} else {
 
-	return cmocka_run_group_tests(tests, NULL, NULL);
+		const struct CMUnitTest tests[] = {
+			cmocka_unit_test(test_normal_update_success),
+			cmocka_unit_test(test_normal_update_failure),
+			cmocka_unit_test(test_delta_dmc_rollback_success),
+			cmocka_unit_test(test_delta_dmc_rollback_failure),
+			cmocka_unit_test(test_rollback_mixed),
+			cmocka_unit_test(test_rollback_fake_version),
+		};
+
+		return cmocka_run_group_tests(tests, NULL, NULL);		
+	}
+
 }
